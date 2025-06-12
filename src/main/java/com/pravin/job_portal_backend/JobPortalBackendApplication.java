546efd1fb9127698c971293a89d3ae2e73e5c9b4
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2025-06-12 21:31:47 +0530

    corrected things

--- src/main/java/com/pravin/job_portal_backend/JobPortalBackendApplication.java
+++ src/main/java/com/pravin/job_portal_backend/JobPortalBackendApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobPortalBackendApplication {
 
   public static void main(String[] args) {
-    // Log key environment variables for debugging
-    System.out.println("DB_URL: " + System.getenv("DB_URL"));
-    System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-    System.out.println("MAIL_USERNAME: " + System.getenv("MAIL_USERNAME"));
-    System.out.println("PORT: " + System.getenv("PORT"));
     SpringApplication.run(JobPortalBackendApplication.class, args);
   }
 
